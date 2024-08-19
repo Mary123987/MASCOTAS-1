95cb6afcba1494ac5148e55144cbe6f546559f28
Mary Rojas <maryrojascordova08@gmail.com>
2024-08-19 12:27:23 -0500

    home viaje commit

--- obj/Debug/net8.0/MASCOTAS.AssemblyInfo.cs
+++ obj/Debug/net8.0/MASCOTAS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MASCOTAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9975d37dd971092169882a30749314880fd9d6d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MASCOTAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MASCOTAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
